abf9b7ef73f5a0f4d6320a245841a98d1769361b
Lu2ky <jaaa736504@gmail.com>
2026-02-22 15:28:09 -0500

    Mini fix

--- API_Front_Back/API.js
+++ API_Front_Back/API.js
@@ -1,18 +1,17 @@
-import {Connection} from "../Connection.js";
-import {fileURLToPath} from "url";
-import {dirname, resolve} from "path";
+import { Connection } from "../Connection.js";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 import dotenv from "dotenv";
-import {officialAct} from "../OfficialAct.js";
-import {PersonalAct} from "../PersonalAct.js";
+import { officialAct } from "../OfficialAct.js";
+import { PersonalAct } from "../PersonalAct.js";
 import express from "express";
 import cors from "cors";
-import {stringify} from "querystring";
+import { stringify } from "querystring";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-dotenv.config({
-	path: resolve(__dirname, "../../../config/expressapiconfig.env")
-});
+//{path: resolve(__dirname, "../../../config/expressapiconfig.env")}
+dotenv.config();
 
 const app = express();
 const PORT = 28523;
@@ -21,173 +20,173 @@ app.use(express.json());
 
 let Con = new Connection();
 app.get("/api/official-schedule/:userId", async (req, res) => {
-	try {
-		const userId = req.params.userId;
-		const activities = await getOfficialScheduleForFront(userId);
+  try {
+    const userId = req.params.userId;
+    const activities = await getOfficialScheduleForFront(userId);
 
-		res.status(200).json({
-			success: true,
-			data: activities
-		});
-	} catch (error) {
-		console.error("Error:", error);
-		res.status(500).json({
-			success: false,
-			message: "Error al obtener el horario",
-			error: error.message
-		});
-	}
+    res.status(200).json({
+      success: true,
+      data: activities,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error al obtener el horario",
+      error: error.message,
+    });
+  }
 });
 
 //getOfficialScheduleForFront("551542");
 
 async function getOfficialScheduleForFront(id) {
-	let data = await Con.GetOfficialScheduleByUserId(id);
-	//console.log(data);
-	let i = 0;
-	let OfficialActivitys = data.map(eachData => {
-		let OfficialActivity = new officialAct(
-			eachData.Course,
-			eachData.Teacher,
-			eachData.Classroom,
-			eachData.Nrc,
-			[eachData.StartHour, eachData.EndHour, eachData.Day],
-			eachData.Tag,
-			eachData.AcademicPeriod,
-			eachData.Campus,
-			{
-				Float64: eachData.Credits.Float64,
-				Valid: eachData.Credits.Valid
-			}
-		);
+  let data = await Con.GetOfficialScheduleByUserId(id);
+  //console.log(data);
+  let i = 0;
+  let OfficialActivitys = data.map((eachData) => {
+    let OfficialActivity = new officialAct(
+      eachData.Course,
+      eachData.Teacher,
+      eachData.Classroom,
+      eachData.Nrc,
+      [eachData.StartHour, eachData.EndHour, eachData.Day],
+      eachData.Tag,
+      eachData.AcademicPeriod,
+      eachData.Campus,
+      {
+        Float64: eachData.Credits.Float64,
+        Valid: eachData.Credits.Valid,
+      },
+    );
 
-		return OfficialActivity.getData();
-	});
-	return OfficialActivitys;
+    return OfficialActivity.getData();
+  });
+  return OfficialActivitys;
 }
 
 app.get("/api/personal-schedule/:userId", async (req, res) => {
-	let data = await Con.GetPersonalScheduleByUserId(req.params.userId);
+  let data = await Con.GetPersonalScheduleByUserId(req.params.userId);
 
-	let PersonalActivitys = data
-		.map(eachData => {
-			console.log(
-				eachData.N_idcourse,
-				eachData.IsDeleted,
-				eachData.IsDeleted.Bool
-			);
+  let PersonalActivitys = data
+    .map((eachData) => {
+      console.log(
+        eachData.N_idcourse,
+        eachData.IsDeleted,
+        eachData.IsDeleted.Bool,
+      );
 
-			if (eachData.IsDeleted.Bool == true) {
-				return null;
-			}
+      if (eachData.IsDeleted.Bool == true) {
+        return null;
+      }
 
-			let PersonalActivity = new PersonalAct(
-				eachData.N_idcourse,
-				eachData.Activity,
-				eachData.Description,
-				[eachData.StartHour, eachData.EndHour, eachData.Day],
-				eachData.Tag,
-				eachData.Dt_Start.String,
-				eachData.Dt_End.String
-			);
+      let PersonalActivity = new PersonalAct(
+        eachData.N_idcourse,
+        eachData.Activity,
+        eachData.Description,
+        [eachData.StartHour, eachData.EndHour, eachData.Day],
+        eachData.Tag,
+        eachData.Dt_Start.String,
+        eachData.Dt_End.String,
+      );
 
-			return PersonalActivity.getData();
-		})
-		.filter(activity => activity !== null);
+      return PersonalActivity.getData();
+    })
+    .filter((activity) => activity !== null);
 
-	return res.json(PersonalActivitys);
+  return res.json(PersonalActivitys);
 });
 
 app.post("/api/update-personal-activity-description", async (req, res) => {
-	/*
+  /*
     data = {
       NewActivityValue: [NEW DESCRIPTION]
       IdCurso: [ID]
     }
   */
-	const NEW_NAME = req.body.NewActivityValue;
-	const ID = req.body.IdPersonalSchedule;
+  const NEW_NAME = req.body.NewActivityValue;
+  const ID = req.body.IdPersonalSchedule;
 
-	console.log(NEW_NAME, ID);
+  console.log(NEW_NAME, ID);
 
-	try {
-		const RESULT = await Con.updateDescriptionOfPersonalScheduleByIdCourse(
-			NEW_NAME,
-			ID
-		);
+  try {
+    const RESULT = await Con.updateDescriptionOfPersonalScheduleByIdCourse(
+      NEW_NAME,
+      ID,
+    );
 
-		const success = RESULT != undefined;
-		return res.status(200).json({
-			success: success
-		});
-	} catch (error) {
-		console.error(error);
-		return res.status(500).json({
-			error: "Error interno del servidor"
-		});
-	}
+    const success = RESULT != undefined;
+    return res.status(200).json({
+      success: success,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: "Error interno del servidor",
+    });
+  }
 });
 
 app.post("/api/update-personal-activity-name", async (req, res) => {
-	/*
+  /*
     data = {
       NewActivityValue: [NEW NAME]
       IdCurso: [ID]
     }
   */
-	const NEW_NAME = req.body.NewActivityValue;
-	const ID = req.body.IdPersonalSchedule;
+  const NEW_NAME = req.body.NewActivityValue;
+  const ID = req.body.IdPersonalSchedule;
 
-	console.log(NEW_NAME, ID);
+  console.log(NEW_NAME, ID);
 
-	try {
-		const RESULT = await Con.updateNameOfPersonalScheduleByIdCourse(
-			NEW_NAME,
-			ID
-		);
+  try {
+    const RESULT = await Con.updateNameOfPersonalScheduleByIdCourse(
+      NEW_NAME,
+      ID,
+    );
 
-		const success = RESULT != undefined;
-		return res.status(200).json({
-			success: success
-		});
-	} catch (error) {
-		console.error(error);
-		return res.status(500).json({
-			error: "Error interno del servidor"
-		});
-	}
+    const success = RESULT != undefined;
+    return res.status(200).json({
+      success: success,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: "Error interno del servidor",
+    });
+  }
 });
 
 app.post("/api/remove-personal-activity", async (req, res) => {
-	/*
+  /*
     data = {
       NewActivityValue: [NEW_STATUS]
       IdCurso: [ID]
     }
   */
-	const NEW_STATUS = req.body.NewActivityValue;
-	const ID = req.body.IdPersonalSchedule;
+  const NEW_STATUS = req.body.NewActivityValue;
+  const ID = req.body.IdPersonalSchedule;
 
-	try {
-		const RESULT = await Con.deleteOrRecoveryPersonalScheduleByIdCourse(
-			NEW_STATUS,
-			ID
-		);
+  try {
+    const RESULT = await Con.deleteOrRecoveryPersonalScheduleByIdCourse(
+      NEW_STATUS,
+      ID,
+    );
 
-		const success = RESULT != undefined;
-		return res.status(200).json({
-			success: success
-		});
-	} catch (error) {
-		console.error(error);
-		return res.status(500).json({
-			error: "Error interno del servidor"
-		});
-	}
+    const success = RESULT != undefined;
+    return res.status(200).json({
+      success: success,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: "Error interno del servidor",
+    });
+  }
 });
 
 app.post("/api/update-personal-activity-time/", async (req, res) => {
-	/*
+  /*
     data = {
       StartHour: [NEW_STA_HOUR],
       EndHour: [NEW_END_HOUR]
@@ -197,46 +196,46 @@ app.post("/api/update-personal-activity-time/", async (req, res) => {
 	  EL TIMES NO PUEDE CONTENER EL TIEMPO DE LA ACTIVIDAD QUE SE QUIERE MODIFICAR
     }
   */
-	const NEW_STA_HOUR = req.body.StartHour;
-	const NEW_END_HOUR = req.body.EndHour;
-	const DAY = req.body.Day;
-	const ID = req.body.IdCurso;
+  const NEW_STA_HOUR = req.body.StartHour;
+  const NEW_END_HOUR = req.body.EndHour;
+  const DAY = req.body.Day;
+  const ID = req.body.IdCurso;
 
-	const TIMES = req.body.Times;
-	const RESULT = [undefined, undefined];
+  const TIMES = req.body.Times;
+  const RESULT = [undefined, undefined];
 
-	console.log(NEW_STA_HOUR, NEW_END_HOUR, DAY, ID);
-	try {
-		if (PersonalAct.hasCollisions(TIMES, NEW_STA_HOUR, NEW_END_HOUR, DAY)) {
-			return res.status(400).json({
-				error: "Colisión de horarios"
-			});
-		}
+  console.log(NEW_STA_HOUR, NEW_END_HOUR, DAY, ID);
+  try {
+    if (PersonalAct.hasCollisions(TIMES, NEW_STA_HOUR, NEW_END_HOUR, DAY)) {
+      return res.status(400).json({
+        error: "Colisión de horarios",
+      });
+    }
 
-		RESULT[0] = await Con.updateStartHourOfPersonalScheduleByIdCourse(
-			NEW_STA_HOUR,
-			ID
-		);
-		RESULT[1] = await Con.updateEndHourOfPersonalScheduleByIdCourse(
-			NEW_END_HOUR,
-			ID
-		);
+    RESULT[0] = await Con.updateStartHourOfPersonalScheduleByIdCourse(
+      NEW_STA_HOUR,
+      ID,
+    );
+    RESULT[1] = await Con.updateEndHourOfPersonalScheduleByIdCourse(
+      NEW_END_HOUR,
+      ID,
+    );
 
-		return res.status(200).json({
-			success: true,
-			NEW_STA_H: RESULT[0],
-			NEW_END_H: RESULT[1]
-		});
-	} catch (error) {
-		console.error(error);
-		return res.status(500).json({
-			error: "Error interno del servidor"
-		});
-	}
+    return res.status(200).json({
+      success: true,
+      NEW_STA_H: RESULT[0],
+      NEW_END_H: RESULT[1],
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: "Error interno del servidor",
+    });
+  }
 });
 
 app.post("/api/add-personal-activity", async (req, res) => {
-	/*
+  /*
     data = {
       name: [NAME],
       tag: [TAG]
@@ -250,56 +249,56 @@ app.post("/api/add-personal-activity", async (req, res) => {
     }
   */
 
-	const NAME = req.body.Activity;
-	const DESC = req.body.Description;
-	const DAY = req.body.Day;
-	const STA_HOUR = req.body.StartHour;
-	const END_HOUR = req.body.EndHour;
-	const ID_USER = req.body.N_iduser;
-	const ID_ACADEMIC_PER = req.body.Id_AcademicPeriod;
+  const NAME = req.body.Activity;
+  const DESC = req.body.Description;
+  const DAY = req.body.Day;
+  const STA_HOUR = req.body.StartHour;
+  const END_HOUR = req.body.EndHour;
+  const ID_USER = req.body.N_iduser;
+  const ID_ACADEMIC_PER = req.body.Id_AcademicPeriod;
 
-	const TIMES = req.body.Times;
+  const TIMES = req.body.Times;
 
-	try {
-		if (PersonalAct.hasCollisions(TIMES, STA_HOUR, END_HOUR, DAY)) {
-			return res.status(400).json({
-				error: "Colisión de horarios"
-			});
-		}
+  try {
+    if (PersonalAct.hasCollisions(TIMES, STA_HOUR, END_HOUR, DAY)) {
+      return res.status(400).json({
+        error: "Colisión de horarios",
+      });
+    }
 
-		const RESULT = await Con.addPersonalActivity(
-			NAME,
-			DESC,
-			DAY,
-			STA_HOUR,
-			END_HOUR,
-			ID_USER,
-			ID_ACADEMIC_PER
-		);
+    const RESULT = await Con.addPersonalActivity(
+      NAME,
+      DESC,
+      DAY,
+      STA_HOUR,
+      END_HOUR,
+      ID_USER,
+      ID_ACADEMIC_PER,
+    );
 
-		return res.status(200).json({
-			success: true,
-			data: RESULT
-		});
-	} catch (error) {
-		console.error(error);
-		return res.status(500).json({
-			error: "Error interno del servidor"
-		});
-	}
+    return res.status(200).json({
+      success: true,
+      data: RESULT,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: "Error interno del servidor",
+    });
+  }
 });
 
 /**
  * Se obtienen las etiquetas como un arreglo.
  */
 app.get("/api/get-tags", async (req, res) => {
-	let data = await Con.GetTags();
+  let data = await Con.GetTags();
 
-	let tags = data.map(eachData => {
-		return eachData.T_name;
-	});
+  let tags = data.map((eachData) => {
+    return eachData.T_name;
+  });
 
-	return res.json(tags);
+  return res.json(tags);
 });
 
 app.listen(PORT);
--- Connection.js
+++ Connection.js
@@ -1,330 +1,331 @@
 import dotenv from "dotenv";
-import {fileURLToPath} from "url";
-import {dirname, resolve} from "path";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-dotenv.config({path: resolve(__dirname, "../../config/expressapiconfig.env")});
+//{path: resolve(__dirname, "../../config/expressapiconfig.env")}
+dotenv.config();
 
 export class Connection {
-	constructor() {}
+  constructor() {}
 
-	async GetOfficialScheduleByUserId(id) {
-		const url =
-			"http://" +
-			process.env.API_ADDR +
-			":" +
-			process.env.API_PORT +
-			"/GetOfficialScheduleByUserId/" +
-			id;
-		try {
-			const rta = await fetch(url, {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-					"X-API-Key": process.env.API_KEY
-				}
-			});
-			if (!rta.ok) throw new Error(`Error: ${rta.status}`);
-			const data = await rta.json();
-			return data;
-		} catch (error) {
-			console.error("Mira este error papu, que raro: ", error);
-		}
-	}
+  async GetOfficialScheduleByUserId(id) {
+    const url =
+      "http://" +
+      process.env.API_ADDR +
+      ":" +
+      process.env.API_PORT +
+      "/GetOfficialScheduleByUserId/" +
+      id;
+    try {
+      const rta = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": process.env.API_KEY,
+        },
+      });
+      if (!rta.ok) throw new Error(`Error: ${rta.status}`);
+      const data = await rta.json();
+      return data;
+    } catch (error) {
+      console.error("Mira este error papu, que raro: ", error);
+    }
+  }
 
-	async GetPersonalScheduleByUserId(id) {
-		let data;
-		const url =
-			"http://" +
-			process.env.API_ADDR +
-			":" +
-			process.env.API_PORT +
-			"/GetPersonalScheduleByUserId/" +
-			id;
-		try {
-			const rta = await fetch(url, {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-					"X-API-Key": process.env.API_KEY
-				}
-			});
-			if (!rta.ok) throw new Error(`Error: ${rta.status}`);
-			const data = await rta.json();
-			return data;
-		} catch (error) {
-			console.error("Mira este error papu, que raro: ", error);
-		}
-	}
+  async GetPersonalScheduleByUserId(id) {
+    let data;
+    const url =
+      "http://" +
+      process.env.API_ADDR +
+      ":" +
+      process.env.API_PORT +
+      "/GetPersonalScheduleByUserId/" +
+      id;
+    try {
+      const rta = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": process.env.API_KEY,
+        },
+      });
+      if (!rta.ok) throw new Error(`Error: ${rta.status}`);
+      const data = await rta.json();
+      return data;
+    } catch (error) {
+      console.error("Mira este error papu, que raro: ", error);
+    }
+  }
 
-	async updateNameOfPersonalScheduleByIdCourse(
-		newActivityValue,
-		idPersonalSchedule
-	) {
-		const url =
-			"http://" +
-			process.env.API_ADDR +
-			":" +
-			process.env.API_PORT +
-			"/updateNameOfPersonalScheduleByIdCourse";
+  async updateNameOfPersonalScheduleByIdCourse(
+    newActivityValue,
+    idPersonalSchedule,
+  ) {
+    const url =
+      "http://" +
+      process.env.API_ADDR +
+      ":" +
+      process.env.API_PORT +
+      "/updateNameOfPersonalScheduleByIdCourse";
 
-		const data = {
-			NewActivityValue: newActivityValue,
-			IdPersonalSchedule: idPersonalSchedule
-		};
+    const data = {
+      NewActivityValue: newActivityValue,
+      IdPersonalSchedule: idPersonalSchedule,
+    };
 
-		try {
-			const send = await fetch(url, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					"X-API-Key": process.env.API_KEY
-				},
-				body: JSON.stringify(data)
-			});
-			const response = await send.json();
+    try {
+      const send = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": process.env.API_KEY,
+        },
+        body: JSON.stringify(data),
+      });
+      const response = await send.json();
 
-			if (send.status == 200) {
-				return send;
-			} else {
-				throw new Error(response.error || "No se q paso papu");
-			}
-		} catch (error) {
-			console.error("Mira este error papu, que raro: ", error);
-		}
-	}
+      if (send.status == 200) {
+        return send;
+      } else {
+        throw new Error(response.error || "No se q paso papu");
+      }
+    } catch (error) {
+      console.error("Mira este error papu, que raro: ", error);
+    }
+  }
 
-	async updateDescriptionOfPersonalScheduleByIdCourse(
-		newActivityValue,
-		idPersonalSchedule
-	) {
-		const url =
-			"http://" +
-			process.env.API_ADDR +
-			":" +
-			process.env.API_PORT +
-			"/updateDescriptionOfPersonalScheduleByIdCourse";
+  async updateDescriptionOfPersonalScheduleByIdCourse(
+    newActivityValue,
+    idPersonalSchedule,
+  ) {
+    const url =
+      "http://" +
+      process.env.API_ADDR +
+      ":" +
+      process.env.API_PORT +
+      "/updateDescriptionOfPersonalScheduleByIdCourse";
 
-		const data = {
-			NewActivityValue: newActivityValue,
-			IdPersonalSchedule: idPersonalSchedule
-		};
+    const data = {
+      NewActivityValue: newActivityValue,
+      IdPersonalSchedule: idPersonalSchedule,
+    };
 
-		try {
-			const send = await fetch(url, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					"X-API-Key": process.env.API_KEY
-				},
-				body: JSON.stringify(data)
-			});
+    try {
+      const send = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": process.env.API_KEY,
+        },
+        body: JSON.stringify(data),
+      });
 
-			const response = await send.json();
+      const response = await send.json();
 
-			if (send.status == 200) {
-				return send;
-			} else {
-				throw new Error(response.error || "Me lleva el chanfle");
-			}
-		} catch (error) {
-			console.error("Mira este error papu, que raro: ", error);
-		}
-	}
+      if (send.status == 200) {
+        return send;
+      } else {
+        throw new Error(response.error || "Me lleva el chanfle");
+      }
+    } catch (error) {
+      console.error("Mira este error papu, que raro: ", error);
+    }
+  }
 
-	async updateStartHourOfPersonalScheduleByIdCourse(
-		newActivityValue,
-		idPersonalSchedule
-	) {
-		const url =
-			"http://" +
-			process.env.API_ADDR +
-			":" +
-			process.env.API_PORT +
-			"/updateStartHourOfPersonalScheduleByIdCourse";
+  async updateStartHourOfPersonalScheduleByIdCourse(
+    newActivityValue,
+    idPersonalSchedule,
+  ) {
+    const url =
+      "http://" +
+      process.env.API_ADDR +
+      ":" +
+      process.env.API_PORT +
+      "/updateStartHourOfPersonalScheduleByIdCourse";
 
-		const data = {
-			NewActivityValue: newActivityValue,
-			IdPersonalSchedule: idPersonalSchedule
-		};
+    const data = {
+      NewActivityValue: newActivityValue,
+      IdPersonalSchedule: idPersonalSchedule,
+    };
 
-		try {
-			const send = await fetch(url, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					"X-API-Key": process.env.API_KEY
-				},
-				body: JSON.stringify(data)
-			});
+    try {
+      const send = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": process.env.API_KEY,
+        },
+        body: JSON.stringify(data),
+      });
 
-			const response = await send.json();
+      const response = await send.json();
 
-			if (send.status == 200) {
-				return send;
-			} else {
-				throw new Error(result.error || "Me lleva el chanfle");
-			}
-		} catch (error) {
-			console.error("Mira este error papu, que raro: ", error);
-		}
-	}
+      if (send.status == 200) {
+        return send;
+      } else {
+        throw new Error(result.error || "Me lleva el chanfle");
+      }
+    } catch (error) {
+      console.error("Mira este error papu, que raro: ", error);
+    }
+  }
 
-	async updateEndHourOfPersonalScheduleByIdCourse(
-		newActivityValue,
-		idPersonalSchedule
-	) {
-		const url =
-			"http://" +
-			process.env.API_ADDR +
-			":" +
-			process.env.API_PORT +
-			"/updateEndHourOfPersonalScheduleByIdCourse";
+  async updateEndHourOfPersonalScheduleByIdCourse(
+    newActivityValue,
+    idPersonalSchedule,
+  ) {
+    const url =
+      "http://" +
+      process.env.API_ADDR +
+      ":" +
+      process.env.API_PORT +
+      "/updateEndHourOfPersonalScheduleByIdCourse";
 
-		const data = {
-			NewActivityValue: newActivityValue,
-			IdPersonalSchedule: idPersonalSchedule
-		};
+    const data = {
+      NewActivityValue: newActivityValue,
+      IdPersonalSchedule: idPersonalSchedule,
+    };
 
-		try {
-			const send = await fetch(url, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					"X-API-Key": process.env.API_KEY
-				},
-				body: JSON.stringify(data)
-			});
+    try {
+      const send = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": process.env.API_KEY,
+        },
+        body: JSON.stringify(data),
+      });
 
-			const response = await send.json();
+      const response = await send.json();
 
-			if (send.status == 200) {
-				return send;
-			} else {
-				throw new Error(result.error || "Me lleva el chanfle");
-			}
-		} catch (error) {
-			console.error("Mira este error papu, que raro: ", error);
-		}
-	}
+      if (send.status == 200) {
+        return send;
+      } else {
+        throw new Error(result.error || "Me lleva el chanfle");
+      }
+    } catch (error) {
+      console.error("Mira este error papu, que raro: ", error);
+    }
+  }
 
-	async deleteOrRecoveryPersonalScheduleByIdCourse(
-		isDeleted,
-		idPersonalSchedule
-	) {
-		const url =
-			"http://" +
-			process.env.API_ADDR +
-			":" +
-			process.env.API_PORT +
-			"/deleteOrRecoveryPersonalScheduleByIdCourse";
+  async deleteOrRecoveryPersonalScheduleByIdCourse(
+    isDeleted,
+    idPersonalSchedule,
+  ) {
+    const url =
+      "http://" +
+      process.env.API_ADDR +
+      ":" +
+      process.env.API_PORT +
+      "/deleteOrRecoveryPersonalScheduleByIdCourse";
 
-		const data = {
-			IsDeleted: isDeleted,
-			IdPersonalSchedule: idPersonalSchedule
-		};
+    const data = {
+      IsDeleted: isDeleted,
+      IdPersonalSchedule: idPersonalSchedule,
+    };
 
-		try {
-			const send = await fetch(url, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					"X-API-Key": process.env.API_KEY
-				},
-				body: JSON.stringify(data)
-			});
+    try {
+      const send = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": process.env.API_KEY,
+        },
+        body: JSON.stringify(data),
+      });
 
-			const response = await send.json();
+      const response = await send.json();
 
-			if (send.status == 200) {
-				return send;
-			} else {
-				throw new Error(response.error);
-			}
-		} catch (error) {
-			console.error("Mira este error papu, que raro: ", error);
-		}
-	}
+      if (send.status == 200) {
+        return send;
+      } else {
+        throw new Error(response.error);
+      }
+    } catch (error) {
+      console.error("Mira este error papu, que raro: ", error);
+    }
+  }
 
-	async addPersonalActivity(
-		activity,
-		description,
-		day,
-		startHour,
-		endHour,
-		n_iduser,
-		id_academicperiod
-	) {
-		const url =
-			"http://" +
-			process.env.API_ADDR +
-			":" +
-			process.env.API_PORT +
-			"/addPersonalActivity";
+  async addPersonalActivity(
+    activity,
+    description,
+    day,
+    startHour,
+    endHour,
+    n_iduser,
+    id_academicperiod,
+  ) {
+    const url =
+      "http://" +
+      process.env.API_ADDR +
+      ":" +
+      process.env.API_PORT +
+      "/addPersonalActivity";
 
-		const data = {
-			Activity: activity,
-			Description: description,
-			N_idTipoCurso: 7,
-			Day: day,
-			StartHour: startHour,
-			EndHour: endHour,
-			N_iduser: n_iduser,
-			Id_AcademicPeriod: id_academicperiod
-		};
+    const data = {
+      Activity: activity,
+      Description: description,
+      N_idTipoCurso: 7,
+      Day: day,
+      StartHour: startHour,
+      EndHour: endHour,
+      N_iduser: n_iduser,
+      Id_AcademicPeriod: id_academicperiod,
+    };
 
-		try {
-			const send = await fetch(url, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					"X-API-Key": process.env.API_KEY
-				},
-				body: JSON.stringify(data)
-			});
+    try {
+      const send = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": process.env.API_KEY,
+        },
+        body: JSON.stringify(data),
+      });
 
-			const response = await send.json();
+      const response = await send.json();
 
-			if (send.status == 200) {
-				return send;
-			} else {
-				throw new Error(response.error || "Me lleva el chanfle");
-			}
-		} catch (error) {
-			console.error("Mira este error papu, que raro: ", error);
-		}
-	}
+      if (send.status == 200) {
+        return send;
+      } else {
+        throw new Error(response.error || "Me lleva el chanfle");
+      }
+    } catch (error) {
+      console.error("Mira este error papu, que raro: ", error);
+    }
+  }
 
-	// TO DO:
+  // TO DO:
 
-	//GET TAGS
-	async GetTags() {
-		let data;
-		const url =
-			"http://" +
-			process.env.API_ADDR +
-			":" +
-			process.env.API_PORT +
-			"/GetTags";
-		try {
-			const rta = await fetch(url, {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-					"X-API-Key": process.env.API_KEY
-				}
-			});
+  //GET TAGS
+  async GetTags() {
+    let data;
+    const url =
+      "http://" +
+      process.env.API_ADDR +
+      ":" +
+      process.env.API_PORT +
+      "/GetTags";
+    try {
+      const rta = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": process.env.API_KEY,
+        },
+      });
 
-			if (!rta.ok) throw new Error(`Error: ${rta.status}`);
+      if (!rta.ok) throw new Error(`Error: ${rta.status}`);
 
-			const data = await rta.json();
-			return data;
-		} catch (error) {
-			console.error("Mira este error papu, que raro: ", error);
-		}
-	}
+      const data = await rta.json();
+      return data;
+    } catch (error) {
+      console.error("Mira este error papu, que raro: ", error);
+    }
+  }
 
-	//GET PERSONAL COMMENTS
+  //GET PERSONAL COMMENTS
 
-	//ADD PERSONAL COMMENT
+  //ADD PERSONAL COMMENT
 }
