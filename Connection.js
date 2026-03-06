a0e1b9bc93699f4059bcdd65db013157d8ecb731
Luky <160178490+Lu2ky@users.noreply.github.com>
2026-03-06 06:30:43 +0000

    Comment out local dotenv config line
    Comment out local dotenv configuration for clarity.

--- Connection.js
+++ Connection.js
@@ -5,10 +5,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 dotenv.config();	//PROD
-dotenv.config({path: resolve(__dirname, "../../config/expressapiconfig.env")});	//LOCAL
+//dotenv.config({path: resolve(__dirname, "../../config/expressapiconfig.env")});	//LOCAL
 
 export class Connection {
-<<<<<<< HEAD
 	constructor() {}
 
 	//	--------------------------------------- ACTIVIDADES -------------------------------------- \\
