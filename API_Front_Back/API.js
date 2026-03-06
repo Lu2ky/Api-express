4cadee9ea68a2bbef9adff7457988d3cca8c465f
Dalyhae <ilovethebabyducklings@gmail.com>
2026-03-05 23:03:39 -0500

    Fix: El envío de correos y notificaciones al crear el recordatorio fue perfeccionado 2

--- API_Front_Back/API.js
+++ API_Front_Back/API.js
@@ -19,7 +19,7 @@ const __dirname = dirname(__filename);
 //INTERCAMBIAR ESTAS DOS LINEAS SI SE QUIERE EJECUTAR EN LOCAL O SI SE SUBIRÁ A PRODUCCION
 
 dotenv.config(); //PROD
-dotenv.config({path: resolve(__dirname, "../../../config/expressapiconfig.env")});	//LOCAL
+// dotenv.config({path: resolve(__dirname, "../../../config/expressapiconfig.env")});	//LOCAL
 
 const app = express();
 const PORT = 28523;
@@ -951,7 +951,7 @@ const scheduleEmailAndNotification = (idToDo, userName, title, content, dateStr,
             }
 
             try {
-                await fetch('http://localhost:28523/api/add-notification', { 
+                await fetch('http://209.25.140.25:9242/api/add-notification', { 
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify(notiDate)
