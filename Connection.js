769610d21d8ef92ff5def16a53cf6104c6dee95a
Luky <160178490+Lu2ky@users.noreply.github.com>
2026-03-23 19:38:24 -0500

    Fix typo in API key environment variable

--- Connection.js
+++ Connection.js
@@ -1261,7 +1261,7 @@ export class Connection {
 			method: "POST",
 			headers: {
 			"Content-Type": "application/json",
-			"X-API-Key": procces.env.API_KEY,
+			"X-API-Key": process.env.API_KEY,
 			},
 			body: JSON.stringify(data),
 		});
