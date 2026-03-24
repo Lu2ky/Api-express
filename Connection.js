240e054234096f64433f19e841710051eb5a67c2
Luky <160178490+Lu2ky@users.noreply.github.com>
2026-03-23 21:28:28 -0500

    Change bcrypt import from bcryptjs to bcrypt

--- Connection.js
+++ Connection.js
@@ -3,7 +3,7 @@ import {fileURLToPath} from "url";
 import {dirname, resolve} from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 
 dotenv.config();	//PROD
 // dotenv.config({path: resolve(__dirname, "../../config/expressapiconfig.env")});	//LOCAL
